2b7174848e012d1896a53571f999eceb046437f8
KaitoZan <zaramakfc@gmail.com>
2025-02-25 12:58:23 +0700

    แก้ไข Six push ไม่หมด

--- src/com/sau/workshop/BMIProject1.java
+++ src/com/sau/workshop/BMIProject1.java
@@ -1,4 +1,31 @@
 package com.sau.workshop;
 
+import java.util.Scanner;
+
 public class BMIProject1 {
+    public static void main(String[] args) {
+        String empName;
+        double weight,heightM,heightCm,BMI ;
+
+        System.out.println("***********************************");
+        System.out.println("      BMI Program by DTI-SAU");
+        System.out.println("***********************************");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input employee name : ");
+        empName = sc.next();
+        System.out.print("Input Weight (kg) : ");
+        weight = sc.nextDouble();
+        System.out.print("Input Hight (cm) : ");
+        heightCm = sc.nextDouble();
+
+        System.out.println("-----------------------------");
+        heightM = heightCm / 100;
+        BMI = (weight/ Math.pow(heightM, 2));
+        System.out.println("Your name : "+empName);
+        System.out.println("Your Weight : "+weight+" (kg) Hight : "+heightCm+(" (cm)"));
+        System.out.println(String.format("BMI: %.2f", BMI));
+
+
+    }
 }
--- src/com/sau/workshop/BMIProject2.java
+++ src/com/sau/workshop/BMIProject2.java
@@ -1,4 +1,38 @@
 package com.sau.workshop;
 
+import  java.io.IOException;
+import java.io.InputStreamReader;
+import  java.io.BufferedReader;
+import java.text.DecimalFormat;
+
 public class BMIProject2 {
+    public static void main(String[] args) throws IOException {
+        String empName;
+        double weight,heightM,heightCm,BMI ;
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        DecimalFormat num = new DecimalFormat("0.000");
+
+
+        System.out.println("***********************************");
+        System.out.println("      BMI Program by DTI-SAU");
+        System.out.println("***********************************");
+
+        System.out.print("Input employee name : ");
+        empName = reader.readLine();
+        System.out.print("Input Weight (kg) : ");
+        weight = Double.parseDouble(reader.readLine());
+        System.out.print("Input Hight (cm) : ");
+        heightCm = weight = Double.parseDouble(reader.readLine());
+
+        System.out.println("-----------------------------");
+        heightM = heightCm / 100;
+        BMI = (weight/ Math.pow(heightM, 2));
+        System.out.println("Your name : "+empName);
+        System.out.println("Your Weight : "+weight+" (kg) Hight : "+heightCm+(" (cm)"));
+        System.out.println("BMI : "+num.format(BMI));
+
+
+    }
 }
